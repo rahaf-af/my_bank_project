e20328c83a86382357c284a6a0d962b5a473a155
rahaf-af <rahaf77553@gmail.com>
2025-09-23 02:19:50 +0300

    The sign up function is done

--- user.py
+++ user.py
@@ -1,19 +1,35 @@
 import sys
 import csv
 import re
+import random
 
-
+user_data = []
 class User:
+    def __init__(self,first_name,last_name,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.id = random.randint(1111,9999)
+
     def signup(self):
-       pass
+        if self.first_name!= "" or self.last_name != "" or self.password != "":
+          user_data.append([self.id,self.first_name,self.last_name,self.password])  
+          with open("bank.csv", "a" ,newline="") as file:
+            writer = csv.writer(file , delimiter=",")
+            writer.writerow(["user_id","first_name","last_name","password","balance_checking","balance_savings"])
+            for row in user_data:
+                writer.writerow(row)
+                return self.id
+       
     def login(self):
         pass
-    def logout(exit_choice):
+
+    def logout(self,choice):
         while True:
-            if exit_choice == "Y":
+            if choice == "Y":
                 print("\nGoodbye, we hope you come back again. 👋")
                 sys.exit()
-            elif exit_choice == "N":
+            elif choice == "N":
                 print("\nOkay we'll take you back to the main menu.")
                 break
             else:
@@ -21,20 +37,27 @@ class User:
                 break
 
 print("\nwelcome!! to our banking system 🏦💰")
+user_input = 0
 while True :
-    user_input = int(input("\nwhat would you like to do ?\n 1)Login 2)Signup 3)Exit "))
-    print(user_input)
-    print(type(user_input))
+    try:
+        user_input = int(input("\nwhat would you like to do ?\n 1)Login 2)Signup 3)Exit "))
+    except ValueError:
+        print("\nwrong choice please try again")
+
     if user_input == 1:
-        fname = input("enter your first name: ")
+        fname = input("\nenter your first name: ")
         lname = input("enter your last name: ")
         password = input("enter your password: ")
+        new_user =User(fname, lname, password)
+        new_user.signup()
+        print(f"\nwe are so happy to have you here {fname} 😊")
+        print(f"your id number is {new_user.id}")
+        break
             
     elif user_input == 2:
         pass
     
     elif user_input == 3:
-        exit_choice=input("\nAre you sure you want to exit the program?\n Y)yes N)no ").upper()
-        User.logout(exit_choice)
-
-        
+        e_choice=input("\nAre you sure you want to exit the program?\n Y)yes N)no ").upper()
+        User.logout(e_choice)
+    
