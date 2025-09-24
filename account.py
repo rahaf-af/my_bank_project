c9e6f63ee9c9c483493ff014e70d11e79a690580
rahaf-af <rahaf77553@gmail.com>
2025-09-24 09:28:19 +0300

    both of the class Account and the deposit function has been craeted

---
+++ account.py
@@ -0,0 +1,15 @@
+import random
+def create_account_num():
+    account_num = random.randint(10000,99999)
+    return account_num
+class Account:
+    def __init__ (self, account_user):
+        self.account_number = create_account_num()
+        self.account_user = account_user
+        self.balance = 0
+    def deposit(self, amount):
+        if amount > 0 :
+            self.balance += amount
+            print(f"The amount you deposited is {amount}, and your new alance is {self.balance} ")
+        else:
+            print("The value you entered is incorrect.")
